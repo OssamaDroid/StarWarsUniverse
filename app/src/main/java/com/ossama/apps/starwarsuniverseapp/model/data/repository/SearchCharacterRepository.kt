e5820765320d5b277614021c3002fb4554c3429e
Ossama <fixed-term.ossama.bounnite@bosch-softtec.com>
2019-04-15 15:38:02 +0200

    Returning liveData from repository's search method

--- app/src/main/java/com/ossama/apps/starwarsuniverseapp/model/data/repository/SearchCharacterRepository.kt
+++ app/src/main/java/com/ossama/apps/starwarsuniverseapp/model/data/repository/SearchCharacterRepository.kt
@@ -1,9 +1,16 @@
 package com.ossama.apps.starwarsuniverseapp.model.data.repository
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import com.ossama.apps.starwarsuniverseapp.model.data.api.APIClient
 import com.ossama.apps.starwarsuniverseapp.model.data.api.service.DataService
+import com.ossama.apps.starwarsuniverseapp.model.entity.SWCharacter
 import com.ossama.apps.starwarsuniverseapp.model.entity.mappingEntity.SearchResponse
+import com.ossama.apps.starwarsuniverseapp.viewModel.SearchCharacterViewModel
+import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
 class SearchCharacterRepository {
 
@@ -11,8 +18,30 @@ class SearchCharacterRepository {
         APIClient.client.create(DataService::class.java)
     }
 
-    fun searchCharacter(name: String, callback: Callback<SearchResponse>) {
+    fun searchCharacter(name: String) : LiveData<List<SWCharacter>?> {
         val call = dataService.searchCharacter(name)
-        call.enqueue(callback)
+
+        val result = MutableLiveData<List<SWCharacter>?>()
+        call.enqueue(object : Callback<SearchResponse> {
+            override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
+                Log.e(SearchCharacterViewModel::class.java.simpleName, t.message)
+                result.postValue(null)
+            }
+
+            override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
+                val responseBody = response.body()
+
+                if (responseBody == null) {
+                    result.postValue(null)
+                    return
+                }
+                val mappedResponse = mutableListOf<SWCharacter>()
+                responseBody.results.forEach {
+                    mappedResponse.add(it.toBaseCharacterInfo())
+                }
+                result.postValue(mappedResponse.toList())
+            }
+        })
+        return result
     }
 }
--- app/src/main/java/com/ossama/apps/starwarsuniverseapp/viewModel/SearchCharacterViewModel.kt
+++ app/src/main/java/com/ossama/apps/starwarsuniverseapp/viewModel/SearchCharacterViewModel.kt
@@ -1,22 +1,17 @@
 package com.ossama.apps.starwarsuniverseapp.viewModel
 
-import android.util.Log
 import androidx.databinding.Bindable
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.MediatorLiveData
 import com.ossama.apps.starwarsuniverseapp.BR
 import com.ossama.apps.starwarsuniverseapp.model.data.repository.SearchCharacterRepository
 import com.ossama.apps.starwarsuniverseapp.model.entity.SWCharacter
-import com.ossama.apps.starwarsuniverseapp.model.entity.mappingEntity.SearchResponse
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class SearchCharacterViewModel(private val repository: SearchCharacterRepository = SearchCharacterRepository()) : ObservableViewModel() {
 
     var input = ""
 
-    private var _characters: MutableLiveData<List<SWCharacter>> = MutableLiveData()
+    private val _characters = MediatorLiveData<List<SWCharacter>?>()
 
     val characters: LiveData<List<SWCharacter>?> by lazy {
         _characters
@@ -27,31 +22,12 @@ class SearchCharacterViewModel(private val repository: SearchCharacterRepository
 
     fun search() {
         if (input.isNotBlank()) {
-            repository.searchCharacter(input, object : Callback<SearchResponse> {
-                override fun onFailure(call: Call<SearchResponse>, t: Throwable) {
-                    Log.e(SearchCharacterViewModel::class.java.simpleName, t.message)
-                    setCharactersValue(null)
-                }
-
-                override fun onResponse(call: Call<SearchResponse>, response: Response<SearchResponse>) {
-                    val responseBody = response.body()
-
-                    if (responseBody == null) {
-                        setCharactersValue(null)
-                        return
-                    }
-                    val mappedResponse = mutableListOf<SWCharacter>()
-                    responseBody.results.forEach{
-                        mappedResponse.add(it.toBaseCharacterInfo())
-                    }
-                    setCharactersValue(mappedResponse.toList())
-                }
-            })
+            val liveData = repository.searchCharacter(input)
+            _characters.addSource(liveData) {
+                _characters.value = it
+                notifyPropertyChanged(BR.listVisible)
+                _characters.removeSource(liveData)
+            }
         }
-    }
-
-    fun setCharactersValue(characters: List<SWCharacter>?) {
-        _characters.value = characters
-        notifyPropertyChanged(BR.listVisible)
     }
 }
